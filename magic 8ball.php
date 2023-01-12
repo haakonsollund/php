e25ea86a1a0dac2a0289fb9859725eddf2ef9778
haakon_sollund <tridentgd@gmail.com>
2023-01-12 14:01:14 +0100

    Create magic 8ball.php

---
+++ magic 8ball.php
@@ -0,0 +1,17 @@
+<?php
+$random_tall = rand (1,10);
+
+if ($random_tall == 1){
+    echo "<p> du blir gal </p>";
+}else if($random_tall == 2){
+    echo "<p> du får en dag med lykke </p>";
+}else if($random_tall == 3){
+    echo "<p> du dør </p>";
+}else {
+    echo "<p> ingen ting skjerr </p>";
+}
+
+
+
+?>
+
