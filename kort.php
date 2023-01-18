1a7788303f5af68f96670f1a91d85a084318dcf2
haakon_sollund <tridentgd@gmail.com>
2023-01-18 12:41:38 +0100

    d

---
+++ kort.php
@@ -0,0 +1,16 @@
+<?php
+
+$kortstokk = array("");
+
+$trokket_kort = $kortstokk[];
+
+$random = rand(0,count($kortstokk))
+
+echo "";
+
+echo count($kortstokk)
+
+?>
+
+
+<img src="2_of_clibs.png>
---
+++ send.php
@@ -0,0 +1,5 @@
+<?php
+$farge = "neon_grønn";
+
+echo "<a href = 'motta.php?farge'>din lenke</a>";
+?>
